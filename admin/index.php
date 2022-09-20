55059a560b588bd9557198fee624b360c7a00fc8
Papatronic <zolmkoz1@gmail.com>
2022-09-20 14:05:03 +0700

    add

--- admin/index.php
+++ admin/index.php
@@ -92,7 +92,7 @@ $admin = getAdmin($_SESSION['admin_auth']);
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="index.php" class="brand-link">
       <img src="../assets/images/icon-3.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">GreenwichGram</span>
     </a>
@@ -263,7 +263,7 @@ if(isset($_GET['edit_profile'])){
                   </div>
                   <div class="form-group">
                     <label for="exampleInputPassword1">Password</label>
-                    <input type="text" name="password" value="<?=$admin['password_text']?>" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    <input type="password" name="password" value="<?=$admin['password_text']?>" class="form-control" id="exampleInputPassword1" placeholder="Password">
                   </div>
                 
                   
