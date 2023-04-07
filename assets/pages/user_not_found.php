e0641a1e17e2d1ada61bdcf06e4e2b6f02cb5c13
Papatronic <zolmkoz1@gmail.com>
2023-04-07 23:20:07 +0700

    -img

--- assets/pages/user_not_found.php
+++ assets/pages/user_not_found.php
@@ -1,3 +1,50 @@
-<div class="container mt-5  border shadow-sm rounded bg-white p-5 d-flex justify-content-center">
-    <h1>User Not Found</h1>
+<div class="container mt-4  border shadow-sm rounded bg-white p-1 d-flex justify-content-center">
+    <div id="main">
+            <div class="fof">
+                    <h1>User Not Found</h1>
+            </div>
+    </div>
 </div>
+
+
+<style>
+
+    
+    *{
+    transition: all 0.6s;
+}
+
+html {
+    height: 50%;
+}
+
+body{
+    font-family: 'Lato', sans-serif;
+    color: #888;
+    margin: 0;
+}
+
+#main{
+    display: table;
+    width:70%;
+    height: 100vh;
+    text-align: center;
+}
+
+.fof{
+	  display: table-cell;
+	  vertical-align: middle;
+}
+
+.fof h1{
+	  font-size: 50px;
+	  display: inline-block;
+	  padding-right: 12px;
+	  animation: type .5s alternate infinite;
+}
+
+@keyframes type{
+	  from{box-shadow: inset -3px 0px 0px #888;}
+	  to{box-shadow: inset -3px 0px 0px transparent;}
+}
+</style>
